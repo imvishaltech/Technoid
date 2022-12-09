82ac0a5018a01df9a0b056f484faf2b6d9c8b6ea
vishal <mevermavishal@gmaol.com>
2022-12-09 19:11:39 +0530

    Technoid basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Expertise from "./routes/Expertise";
+import Contact from "./routes/Contact";
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Expertise" element={<Expertise />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </>
+    );
 }
 
 export default App;
+
+/* this our parrent the app.js, we will call children pages(Home, Project, About us and Contact Us page here) */
